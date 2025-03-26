f64e546452bc393d2fe5c6fd5b4b00419d9517c4
TMQ3105 <quanminhtran3105@gmail.com>
2025-03-26 16:27:35 +0700

    Xóa Form thêm liên hệ

--- ContactList/MainForm.cs
+++ ContactList/MainForm.cs
@@ -19,8 +19,8 @@ namespace ContactList
 
         private void button1_Click(object sender, EventArgs e)
         {
-            AddContactForm addContactForm = new AddContactForm();
-            addContactForm.ShowDialog();
+            
+            
         }
     }
 
